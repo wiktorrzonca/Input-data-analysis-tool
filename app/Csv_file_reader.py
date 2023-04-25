9dae3f7945f3a28e788d45aaa905d3a91faf3ea3
hendrykik <Hendrykik@wp.pl>
2023-04-25 18:23:04 +0200

    sprawdzenie czy są wszystkie pliki .json dla .csv

--- app/Csv_file_reader.py
+++ app/Csv_file_reader.py
@@ -13,3 +13,11 @@ class Csv_file_reader:
     def extract(self):
         with tarfile.open(self.path_to_tar, 'r:gz') as tar_ref:
             tar_ref.extractall('csv_files')
+
+    def check_files(self):
+        missing_files = []
+        for file in os.listdir(self.path_to_tar + '/csv_files'): #otworzenie katalogu csv_files
+                file_json = os.path.splitext(file)[0] + '.json' #zmiana rozszezenia na json
+                if not os.path.isfile(self.path_to_tar + file_json):
+                    missing_files.append(file) 
+        return missing_files #zwracana jest lista plikow ktorych brakuje, jak są wszystkie to zwraca pusta liste
